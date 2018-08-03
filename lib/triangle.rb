d57c3eb35c8d35c8b835f1a2bcad17c73ed31481
Koy Saeteurn <kosatun@aol.com>
2018-08-03 03:26:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,4 +6,24 @@ class Triangle
     @b = b 
     @c = c 
   end   
+  
+  def kind(triangle)
+    if !triangle.valid 
+      begin 
+        raise TriangleError 
+      rescue TriangleError => error 
+        puts error.message 
+      end   
+    end 
+  end 
+  
+  def valid?(triangle)
+    a^2 + b^2 = c^2 
+  end   
 end
+
+  class TriangleError < StandardError 
+    
+  end   
+  
+  
